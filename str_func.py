b574f3f3891dd653fea2b091d8d4263e9242727e
Dmitry Dronov <cskafan1911@gmail.com>
2023-07-30 20:26:16 +0300

    add new function in str_func

--- str_func.py
+++ str_func.py
@@ -1,6 +1,18 @@
-def changes_letters_to_uppercase(value:str):
+def changes_letters_to_uppercase(value: str):
     """
     Принимает строку и возвращает со всеми заглавными буквами
     """
 
     return value.title()
+
+
+def makes_the_first_letters_uppercase(value: str):
+    """
+    Делает первые буквы каждого слова заглавными
+    """
+    value_change = ""
+
+    for word in value.split(" "):
+        value_change += word.lower().title() + " "
+
+    return value_change
