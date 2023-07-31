2a6f07e430d4495f93a36ba2b68963f788b4d62e
cskafan1911 <139268367+cskafan1911@users.noreply.github.com>
2023-07-31 21:26:29 +0300

    Update str_func.py
    Функцию удалил

--- str_func.py
+++ str_func.py
@@ -17,12 +17,3 @@ def makes_the_first_letters_uppercase(value: str):
         value_change += word.lower().title() + " "
 
     return value_change
-
-
-def gets_the_area_of_the_circle(radius: int):
-    """
-    Считает площадь круга и возвращает значение
-    """
-    square_ = 3.14 * radius ** 2
-
-    return square_
